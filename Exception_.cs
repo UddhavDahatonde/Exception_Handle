68cb0e5574e392fd28f6697bc4a217e8f2d9bc35
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-10-08 17:04:23 +0530

    Add project files.

---
+++ Exception_.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception_Handle
+{
+    //internal class Exception_
+    //{
+    //    static void Main()
+    //    {
+    //        try
+    //        {
+    //            Console.WriteLine("Enter the number");
+    //            int a = Convert.ToInt32(Console.ReadLine());
+    //            int b = Convert.ToInt32(Console.ReadLine());
+    //            int c = a / b;
+    //            Console.WriteLine(c);
+    //        }
+    //        catch(Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //        finally
+    //        {
+    //            Console.WriteLine("Hello");
+    //        }
+    //    }
+    //}
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exception_Handle
+{
+    //namespace ClassDemos
+    //{
+        // custom / user defined exception
+    //    public class AgeException : Exception
+    //    {
+    //        public AgeException(string msg) : base(msg)
+    //        {
+    //        }
+
+    //    }
+    //    public class User
+    //    {
+    //        public void AcceptAge(int age)
+    //        {
+    //            if (age < 18)
+    //            {
+    //                throw new AgeException("Your age is not valid for vote, ");
+    //            }
+    //        }
+    //    }
+    //    public class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            User user = new User();
+    //            try
+    //            {
+    //                Console.WriteLine("Enter the the age");
+    //                int age = Convert.ToInt32(Console.ReadLine());
+    //                user.AcceptAge(age);
+    //            }
+    //            catch (AgeException ex)
+    //            {
+    //                Console.WriteLine(ex.Message);
+    //            }
+
+    //        }
+
+    //    }
+    //}
+
+
+}
